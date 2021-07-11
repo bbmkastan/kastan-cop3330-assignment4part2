0a650ef60039e6ca174d3f1f03088b2f0b12fde9
Bao Kastan <bbmkastan@gmail.com>
2021-07-11 06:15:43 -0400

    Initial commit - made GUI for program

---
+++ src/main/java/ucf/assignments/ListAppController.java
@@ -0,0 +1,71 @@
+package ucf.assignments;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Bao Kastan
+ */
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class ListAppController {
+
+    @FXML
+    private ListView<?> listOfItems;
+
+    @FXML
+    private DatePicker dueDatePicker;
+
+    @FXML
+    private TextField itemDescriptionField;
+
+    @FXML
+    void clearAllButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void createNewItemButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void helpButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveAsButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showAllButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showOnlyCompletedButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showOnlyIncompletedButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toggleCompleteButtonClicked(ActionEvent event) {
+
+    }
+
+}
+
